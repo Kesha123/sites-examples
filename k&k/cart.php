9047379c6c16fca30214f04d8e7cf55069cb0886
Kesha123 <33574569+Kesha123@users.noreply.github.com>
2020-08-25 17:24:38 +0300

    Delete cart.php

--- k&k/cart.php
+++
@@ -1,4 +0,0 @@
-<div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
